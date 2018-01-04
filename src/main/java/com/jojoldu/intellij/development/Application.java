b8752e4f38bb6fa9cf03820caa712b0c37d16510
jojoldu <jojoldu@gmail.com>
2018-01-05 08:36:01 +0900

    파일명 수정

--- src/main/java/com/jojoldu/intellij/development/Application.java
+++ src/main/java/com/jojoldu/intellij/development/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntellijDevelopmentApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IntellijDevelopmentApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
