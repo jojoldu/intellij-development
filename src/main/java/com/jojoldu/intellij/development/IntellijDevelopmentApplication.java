575206618b864816e1a8333527d60d94ce9b55b0
이동욱 <dwlee@woowahan.com>
2018-01-05 08:26:19 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/intellij/development/IntellijDevelopmentApplication.java
@@ -0,0 +1,12 @@
+package com.jojoldu.intellij.development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellijDevelopmentApplication.class, args);
+	}
+}
