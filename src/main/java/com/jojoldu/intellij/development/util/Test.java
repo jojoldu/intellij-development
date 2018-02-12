870f3d39460446e2105188c6c50666f30a3313e8
jojoldu <jojoldu@gmail.com>
2018-02-12 21:35:20 +0900

    [ST-1261] [테스트] Task 테스트

---
+++ src/main/java/com/jojoldu/intellij/development/util/Test.java
@@ -0,0 +1,13 @@
+package com.jojoldu.intellij.development.util;
+
+/**
+ * Created by jojoldu@gmail.com on 2018. 2. 12.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+public class Test {
+    public void test() {
+
+    }
+}
