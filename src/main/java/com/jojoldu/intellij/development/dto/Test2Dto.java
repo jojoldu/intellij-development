1bbdf65b691d98030fab6ef7c9f8227bb915e85e
jojoldu <jojoldu@gmail.com>
2018-02-14 12:18:07 +0900

    [ST-1277] [테스트] Intellij & Gitflow 테스트

---
+++ src/main/java/com/jojoldu/intellij/development/dto/Test2Dto.java
@@ -0,0 +1,13 @@
+package com.jojoldu.intellij.development.dto;
+
+/**
+ * Created by jojoldu@gmail.com on 2018. 2. 14.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+public class Test2Dto {
+    public void print() {
+        System.out.println("aaas");
+    }
+}
