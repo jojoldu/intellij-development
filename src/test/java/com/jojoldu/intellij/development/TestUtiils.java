57fffdb45208221aa6bcab656aabc76ba716141e
jojoldu <jojoldu@gmail.com>
2018-02-23 20:02:27 +0900

    [TPBMPG-1574] [테스트] Intellij & Gitflow 사용하기

---
+++ src/test/java/com/jojoldu/intellij/development/TestUtiils.java
@@ -0,0 +1,13 @@
+package com.jojoldu.intellij.development;
+
+/**
+ * Created by jojoldu@gmail.com on 2018. 2. 23.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+public class TestUtiils {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
