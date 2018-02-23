2e71c2631d082ab97395b44b0f1a16e3b272124d
jojoldu <jojoldu@gmail.com>
2018-02-23 20:14:37 +0900

    코드리뷰!

--- src/test/java/com/jojoldu/intellij/development/TestUtiils.java
+++ src/test/java/com/jojoldu/intellij/development/TestUtiils.java
@@ -9,5 +9,7 @@ package com.jojoldu.intellij.development;
 public class TestUtiils {
     public static void main(String[] args) {
         System.out.println("aaa");
+        System.out.println("aaa");
+        System.out.println("aaa");
     }
 }
