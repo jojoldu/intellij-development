2e7762f60367ed4addbebf6d1e7cac7b8921ce32
jojoldu <jojoldu@gmail.com>
2018-02-24 13:57:51 +0900

    [TPBMPG-1573] [테스트] IntelliJ에서 사용하기

---
+++ src/main/java/com/jojoldu/intellij/development/web/Test.java
@@ -0,0 +1,13 @@
+package com.jojoldu.intellij.development.web;
+
+/**
+ * Created by jojoldu@gmail.com on 2018. 2. 24.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
